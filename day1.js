e720d48edf9162d4c22c0df9e4e5f8f22764383c
Othniel Olowe <Oloweothniel09@gmail.com>
2022-12-04 01:55:48 +0100

    Day 1 Chanllege Solution

---
+++ day1.js
@@ -0,0 +1,8 @@
+// alert("working!")
+const btn = document.querySelector("button")
+const body = document.querySelector("body")
+
+btn.addEventListener("click", () => {
+      body.classList.toggle("dark-mode")
+    //   btn.style.backgroundColor("teal")
+})
